ab7ff412d212d7214762579317502aef7944f9f6
Pieter-1337 <pieterbracke@msn.com>
2018-05-04 15:57:01 +0200

    Added Werknemer controller and view also added palindroom view to Werknemer controller

---
+++ MVCVoorbeeld2/Controllers/WerknemerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCVoorbeeld2.Controllers
+{
+    public class WerknemerController : Controller
+    {
+        
+
+        // GET: Werknemer
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Palindroom(string woord)
+        {
+            char[] omgekeerd = woord.ToCharArray();
+            Array.Reverse(omgekeerd);
+            string Achtersteveuren = new string(omgekeerd);
+
+            if(woord == Achtersteveuren)
+            {
+                ViewBag.palindroom = true;
+            }
+            else
+            {
+                ViewBag.palindroom = false;
+            }
+
+            ViewBag.ingetiktwoord = woord;
+
+            return View();
+        }
+    }
+}
